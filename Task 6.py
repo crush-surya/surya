d83a1abfc9810629503291a912bdb26fe9278895
crush-surya <crushsurya2003@gmail.com>
2025-11-03 17:08:33 +0530

    Add files via upload

---
+++ Task 6.py
@@ -0,0 +1,32 @@
+for i in range(1,20):
+    print(i)
+
+for i in range(2,50,2):
+    print(i)
+    
+for i in range (1,50,2):
+    print(i)
+
+for i in range(1,15):
+    print(i*i)
+
+for i in range(1,10):
+    print(i*i*i)
+
+for i in range(10,0,-1):
+    print(i)
+
+num =5
+for i in range(1,11):
+    print(f"{i} x {num} ={i*5}")
+
+name="surya"
+for i in name:
+    print(i)
+    
+for i in range(3,30,3):
+    print(i)
+       
+
+
+
