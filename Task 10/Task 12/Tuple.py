3dc766af7fec2e1a59a321ad75caa72e403680d1
crush-surya <crushsurya2003@gmail.com>
2025-11-12 13:59:56 +0530

    Add files via upload

---
+++ Task 10/Task 12/Tuple.py
@@ -0,0 +1,46 @@
+c=("Red","orange","yellow","green","blue")
+print(c)
+print()
+
+c=("Red","orange","yellow","green","blue")
+print("1st colour :",c[0] and "2nd colour :",c[4])
+print()
+
+c=("Red","orange","yellow","green","blue")
+print("Find the length :",len(c))
+print()
+
+c=("Red","blue","yellow","yellow","blue")
+s = c.count("blue")
+print(s)
+print()
+
+c=("Red","orange","yellow","green","blue")
+for c in c:
+    print(c)
+print()
+
+
+a=("Red","orange")
+b=("yellow","green","blue")
+c=a+b
+print(c)
+print()
+
+a =10
+b=20
+if a>b:
+    print("the maximum is : a")
+else:
+    print("the maximum is : b")
+print()
+
+c = ("Red", "blue", "yellow", "yellow", "blue")
+c[0] = "green"
+
+
+    
+
+
+
+
---
+++ Task 10/Task 12/list.py
@@ -0,0 +1,46 @@
+l =["apple","orange","banana","dragon bruits","mango"]
+print(l)
+print()
+
+a =["apple","orange","banana","dragon bruits"]
+b =["Mango"]
+c=a+b
+print(c)
+print()
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+a.remove("banana")
+print(a)
+print()
+
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+print("count the fruits :",len(a))
+print()
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+for l in l:
+    print(l)
+print()
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+a.reverse()
+print(a)
+print()
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+a.sort()
+print(a)
+print()
+
+
+a =["apple","orange","banana","dragon bruits","Mango"]
+if "apple" in a:
+    print("true")
+else:
+    print("false")
+    
+
+
+
+
